63c48fde50d12b8b659212c266c0cd2cb9791757
PROPHESSOR <xenteze@gmail.com>
2020-03-15 22:33:42 +0200

    [Webpack] Removed sourcemap creation

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -75,7 +75,7 @@ if (process.env.NODE_ENV === 'production') {
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
+      sourceMap: false,
       compress: {
         warnings: false
       }
