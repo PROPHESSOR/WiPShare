c0ed44eef0e67abd5478c32753c39065dad67eaf
PROPHESSOR <xenteze@gmail.com>
2020-03-16 14:27:43 +0200

    Последние исправления (надеюсь)

--- app/src/WifiListItem.vue
+++ app/src/WifiListItem.vue
@@ -2,7 +2,8 @@
   <v-ons-list-item expandable :expanded.sync="isExpanded">
     {{ wlan.SSID || wlan.BSSID }}
     <div class="expandable-content">
-      <pre>{{ JSON.stringify(wlan0, 4) }}</pre></div>
+      <pre>{{ JSON.stringify(wlan0, 0, 4) }}</pre>
+    </div>
   </v-ons-list-item>
 </template>
 
