980c18899c56bfab075090baa108b75ab83dbfd9
PROPHESSOR <xenteze@gmail.com>
2020-03-16 13:42:40 +0200

    [TS] Добавлен интерфейс WLAN

---
+++ app/src/types.d.ts
@@ -0,0 +1,11 @@
+interface WLAN {
+  BSSID: string;
+  SSID: string;
+  capabilities: string;
+  centerFreq0: number;
+  centerFreq1: number;
+  channelWidth: number;
+  frequency: number;
+  level: number;
+  timestamp: number;
+}
